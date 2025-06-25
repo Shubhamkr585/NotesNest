0d537fcd7d7480becd9f8d398d003f78674433a0
Shubhamkr585 <shubhamprd585@gmail.com>
2025-06-25 23:19:27 +0530

    update

--- client/src/components/About.jsx
+++ client/src/components/About.jsx
@@ -2,7 +2,15 @@ import React from 'react'
 
 function About() {
   return (
-    <div>About</div>
+    <div className='flex flex-col items-center justify-center min-h-screen bg-gray-100 p-4'>
+        <h2 className='text-2xl'>About us</h2>
+        <p className='text-lg mt-4 max-w-2xl text-center'>
+            This is a platform built for all the students who are preparing for competitive exams like UPSC and JEE.
+             The aim of the platform is to let student get the notes of ones who have already aces these exams.
+             The toppers of these exams would also share important tricks that they have used .
+        </p>
+
+    </div>
   )
 }
 
